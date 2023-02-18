3640a3d662716593c7a407c047a3e24e50b83363
Wagner Ricardo de Paula Marcolongo <122945519+WagnerRicardo@users.noreply.github.com>
2023-02-18 12:11:47 -0300

    Exercicios 1 e 2
    exercicios de introdução os js e variáveis

---
+++ Ex-1/js/index.js
@@ -0,0 +1,6 @@
+//alert('Message test')
+/*
+    Selecionar/pegar um elemento por seu ID
+    mudar seu valor de string
+*/
+document.getElementById('teste').value = 'Muda'
---
+++ Ex-2/js/index.js
@@ -0,0 +1,15 @@
+//string
+var texto = 'Teste de String'
+
+//number
+var NumInt = -3
+var NumFloat = 40.70
+
+//boolean
+var teste = true
+
+//alert(NumFloat)
+
+//document.write(texto)
+
+console.log(NumFloat + NumInt)
