50589201a3ebdfa2f21380a8ae47bf778d00fa6e
Wagner Ricardo de Paula Marcolongo <122945519+WagnerRicardo@users.noreply.github.com>
2023-02-20 15:57:32 -0300

    exercicios 3 e 4
    mais sobre variaveis

---
+++ Ex-3/js/index.js
@@ -0,0 +1,7 @@
+var username = prompt('Digite seu nome:')
+var userage = prompt('digite sua idade:')
+
+console.log(userage)
+
+document.write('<h1>Olá ' + username + ', Bão? Vi que você tem ' +
+ userage +' anos</h1>')
---
+++ Ex-4/js/index.js
@@ -0,0 +1,22 @@
+//null
+var var1 = null
+
+//undefined
+var var2
+
+console.log(var1)
+console.log(var2)
+
+document.write('<h4>' + var1 + ' ' + var2 + '</h4>')
+
+var1 = 'strings'
+console.log(var1)
+
+document.write('<br>')
+document.write(var1)
+
+var2 = 55.74
+console.log(var2);
+
+document.write('<br>')
+document.write(var2)
