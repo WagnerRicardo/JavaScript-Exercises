db3aa22ea0bc6740ceb2b5356881eb0ed63e83d1
Wagner Ricardo de Paula Marcolongo <122945519+WagnerRicardo@users.noreply.github.com>
2023-02-21 17:01:23 -0300

    Exercicios 5 e 6
    Introdução as condicionais

---
+++ Ex-6/js/index.js
@@ -0,0 +1,56 @@
+//Igualdade
+if (2 == 1+1){
+    document.write('Bloco if (verdade)')
+} else {
+    document.write('Bloco else (falso)')
+}
+document.write('<hr>')
+//Igualdade de valor E tipo
+if (2 === '2'){
+    document.write('Bloco if (verdade)')
+} else {
+    document.write('Bloco else (falso)')
+}
+document.write('<hr>')
+//diferença
+if ('falso' != 'falso'){
+    document.write('Bloco if (verdade)')
+} else {
+    document.write('Bloco else (falso)')
+}
+document.write('<hr>')
+//diferença de valor e tipo
+if (1 !== 2){
+    document.write('Bloco if (verdade)')
+} else {
+    document.write('Bloco else (falso)')
+}
+document.write('<hr>')
+//comparação de maior que
+if ('falsos' > 'falso'){
+    document.write('Bloco if (verdade)')
+} else {
+    document.write('Bloco else (falso)')
+}
+document.write('<hr>')
+//comparação de menor que
+if ('falso' < 'falso'){
+    document.write('Bloco if (verdade)')
+} else {
+    document.write('Bloco else (falso)')
+}
+document.write('<hr>')
+//maior ou igual
+if ('falso' >= 'falso'){
+    document.write('Bloco if (verdade)')
+} else {
+    document.write('Bloco else (falso)')
+}
+document.write('<hr>')
+//menor ou igual
+if (13 <= 14){
+    document.write('Bloco if (verdade)')
+} else {
+    document.write('Bloco else (falso)')
+}
+document.write('<hr>')
