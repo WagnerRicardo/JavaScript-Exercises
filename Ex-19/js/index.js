909edadff7511896136184c0be831740545cd965
Wagner Ricardo de Paula Marcolongo <122945519+WagnerRicardo@users.noreply.github.com>
2023-03-09 19:35:52 -0300

    Exercicio 19 e 20
    Funções para operações matemáticas e funções para datas

---
+++ Ex-19/js/index.js
@@ -0,0 +1,15 @@
+var x = Math.ceil(10.22)
+
+document.write(x + '<br />')
+
+var x = Math.floor(10.22)
+
+document.write(x + '<br />')
+
+var x = Math.round(10.52)
+
+document.write(x + '<br />')
+
+var x = Math.random()
+
+document.write(x + '<br />')
---
+++ Ex-20/js/index.js
@@ -0,0 +1,11 @@
+var data = new Date()
+
+document.write(data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear() + '<hr />')
+
+/*var hour = data.getHours()
+var minute = data.getMinutes()
+var seconds = data.getSeconds()
+var ms = data.getMilliseconds()
+
+document.write(hour + ':' + minute + ':' + seconds + ':' + ms)
+*/
