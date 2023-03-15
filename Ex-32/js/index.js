c7a678a2d67686803fbec28bb1dc6412f6700dbc
Wagner Ricardo de Paula Marcolongo <122945519+WagnerRicardo@users.noreply.github.com>
2023-03-15 14:25:33 -0300

    ex-32
    app calculadora 2

--- Ex-32/js/index.js
+++ Ex-32/js/index.js
@@ -1,3 +1,43 @@
-function calc(type, value){
-    
+//display visual e calculo
+function visor(type, value){
+    //clear
+    if (type === 'act' && value === 'c'){
+        document.getElementById('visor').value = ''
+        storeValues = ''
+    }
+    var visorVal = document.getElementById('visor').value
+    //expressão
+    //ação
+    if (type === 'act' && visorVal.length >= 1 && visorVal.charAt(visorVal.length - 1) !== '/'
+    && visorVal.charAt(visorVal.length - 1) !== '*' && visorVal.charAt(visorVal.length - 1) !== '+'
+    && visorVal.charAt(visorVal.length - 1) !== '.'){
+        if(value === '/'){
+            document.getElementById('visor').value += value
+            storeValues += value
+        }else if(value === '+'){
+            document.getElementById('visor').value += value
+            storeValues += value
+        }
+        else if ( value === '-' ){
+            document.getElementById('visor').value += value
+            storeValues += value
+        }
+        else if(value === '.'){
+            document.getElementById('visor').value += value
+            storeValues += value
+        }
+        else if(value === '*'){
+            storeValues += '*'
+            document.getElementById('visor').value += 'x'
+        }
+        else if (value === '='){
+            document.getElementById('visor').value = eval(storeValues)
+        }
+    //numero
+    }else if (type === 'num'){
+        document.getElementById('visor').value += value
+        storeValues += value
+    }
 }
+//armazenamento
+var storeValues = ''
