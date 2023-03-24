2ab0a5e7b766f5741689231d90ee052fa4f363ea
Wagner Ricardo de Paula Marcolongo <122945519+WagnerRicardo@users.noreply.github.com>
2023-03-24 13:43:26 -0300

    introdução a estruturas de repetição

--- Ex-39/js/index.js
+++ Ex-39/js/index.js
@@ -1,9 +1,29 @@
+function isPrime(number){
+    if (number <= 2){
+        return true
+    }else{
+        var i = 0
+        var bool = true
+        while (i <= 10){
+            if(number%i == 0 && i > 1 && i !== number){
+                bool = false
+            }
+            i++
+        }
+        if (bool === true){
+            return true
+        }else{
+            return false
+        }
+    }
+}
+
 var i = 1
 
 document.write('Lista <br />')
 
-while (i <= 10){
-    if (i%2 !== 0){
+while (i <= 100){
+    if (isPrime(i) === true){
         document.write(i + '<br />')
     }
     i++
